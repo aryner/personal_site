9b567c5e9d06d1afac0c966eab1869180d15042b
aryner <aryner@gmail.com>
2016-04-18 23:07:11 -0700

    added models needed for blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+def Post(models.Model):
+  title = models.CharField(max_length=128,unique=True)
+  title_link = models.URLField()
+  subtitle = models.CharField(max_length=128)
+  date = models.DateField()
+  content = models.TextField()
+
+def Speaker(models.Model):
+  name = models.CharField(max_length=128,unique=True)
+  link = models.URLField()
+  posts = models.ManyToManyField(Post)
+
+def Location(models.Model):
+  name = models.CharField(max_length=128,unique=True)
+  link = models.URLField()
+  posts = models.ManyToManyField(Post)
+
+def Event(models.Model):
+  name = models.CharField(max_length=128,unique=True)
+  link = models.URLField()
+  posts = models.ManyToManyField(Post)
+  
+
