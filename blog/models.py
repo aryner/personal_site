176caa98d8389807583d5dd9f50b96aec921bdd5
aryner <aryner@gmail.com>
2016-04-18 23:15:52 -0700

    models are classes, not functions

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
-def Post(models.Model):
+class Post(models.Model):
   title = models.CharField(max_length=128,unique=True)
   title_link = models.URLField()
   subtitle = models.CharField(max_length=128)
   date = models.DateField()
   content = models.TextField()
 
-def Speaker(models.Model):
+class Speaker(models.Model):
   name = models.CharField(max_length=128,unique=True)
   link = models.URLField()
   posts = models.ManyToManyField(Post)
 
-def Location(models.Model):
+class Location(models.Model):
   name = models.CharField(max_length=128,unique=True)
   link = models.URLField()
   posts = models.ManyToManyField(Post)
 
-def Event(models.Model):
+class Event(models.Model):
   name = models.CharField(max_length=128,unique=True)
   link = models.URLField()
   posts = models.ManyToManyField(Post)
