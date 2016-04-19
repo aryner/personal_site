4fc528e3845acabf80b7dda96ff9425c668bcea1
aryner <aryner@gmail.com>
2016-04-19 13:51:59 -0700

    the blog index view now recieves the 5 most recent posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post, Speaker, Location
+
 # Create your views here.
 def index(request):
-  return render(request,'blog/index.html',{})
+  posts = Post.objects.order_by('date')[:5]
+  base_context = {'posts':posts}
+
+  return render(request,'blog/index.html',base_context)
 
 def post(request):
   return render(request,'blog/post.html',{})
